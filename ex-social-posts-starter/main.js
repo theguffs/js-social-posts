acf0caf27767909057e62931f9b7cb5b15602340
theguffs <youssefabdelmalak01@gmail.com>
2024-08-26 17:46:26 +0200

    (milestone 1 gentilmente offerta da Alessio) milestone 2 dopo secoli

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -55,3 +55,54 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+// lavoro miooooo
+
+// Visualizza l'array di post come una tabella nel console log (visto nella lezione del pomeriggioooo)
+console.table(posts);
+
+const container = document.getElementById('container');
+
+for (let i = 0; i < posts.length; i++) {
+    const post = posts[i];
+    const postElement = document.createElement('div');
+    postElement.classList.add('post');
+
+    // Dichiarazione della variabile per l'HTML dell'immagine del profilo
+    let profileImageHTML = '';
+
+    //un if per controllare se l'immagine del profilo esiste (mannaggia a luca formicola)
+    if (post.author.image) {
+        profileImageHTML = `<img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">`;
+    }
+
+    postElement.innerHTML = `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">${profileImageHTML}</div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.author.name}</div>
+                    <div class="post-meta__time">${post.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${post.content}</div>
+        <div class="post__image">
+            <img src="${post.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button js-like-button" href="#" data-postid="${post.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>
+    `;
+
+    container.appendChild(postElement);
+}
